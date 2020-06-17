29d7b3bd0421c4f1aaa3bfaeb4b6f05b8f1690d4
wolf-coder <cuore.fakhri@gmail.com>
2020-06-17 10:52:14 +0100

    fixing

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,32 +7,42 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *Element = NULL;
 	listint_t *P = NULL, *N = NULL, *Tmp = NULL;
+	listint_t *h = *list;
+	size_t n = 0;
 
-	Element = (*list)->next;
-	while (Element)
+	while (h != NULL)
 	{
-		P = Element->prev;
-		N = P->next;
-		while (P && P->n > Element->n)
-		{
-			Tmp = N->next;
-			N->next = P;
-			P->next = Tmp;
+		n++;
+		h = h->next;
+	}
 
-			Tmp = P->prev;
-			P->prev = N;
-			N->prev = Tmp;
-			if (!N->prev)
-				(*list) = N;
-			else
-				N->prev->next = N;
-			if (P->next)
-				P->next->prev = P;
-			print_list(*list);
-			P = N->prev;
-			if (P)
-				N = P->next;
+	if (n >= 2)
+	{
+		Element = (*list)->next;
+		while (Element)
+		{
+			P = Element->prev;
+			N = P->next;
+			while (P && P->n > Element->n)
+			{
+				Tmp = N->next;
+				N->next = P;
+				P->next = Tmp;
+				Tmp = P->prev;
+				P->prev = N;
+				N->prev = Tmp;
+				if (!N->prev)
+					(*list) = N;
+				else
+					N->prev->next = N;
+				if (P->next)
+					P->next->prev = P;
+				print_list(*list);
+				P = N->prev;
+				if (P)
+					N = P->next;
+			}
+			Element = Element->next;
 		}
-		Element = Element->next;
 	}
 }
