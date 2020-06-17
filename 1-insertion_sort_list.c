9ad7c475653d67d47bcd7ee3606fa69f9a3ddbb1
wolf-coder <cuore.fakhri@gmail.com>
2020-06-17 11:45:39 +0100

    fix

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -17,40 +17,40 @@ void insertion_sort_list(listint_t **list)
 	listint_t *h = *list;
 	size_t n = 0;
 
-	if (list == NULL)
+	if (list == NULL || *list == NULL)
 		return;
-	while (h != NULL)
-	{
-		n++;
-		h = h->next;
-	}
-	if (n >= 2)
-	{
-		Element = (*list)->next;
-		while (Element)
-		{
-			P = Element->prev;
-			N = P->next;
-			while (P && P->n > Element->n)
-			{
-				Tmp = N->next;
-				N->next = P;
-				P->next = Tmp;
-				Tmp = P->prev;
-				P->prev = N;
-				N->prev = Tmp;
-				if (!N->prev)
-					(*list) = N;
-				else
-					N->prev->next = N;
-				if (P->next)
-					P->next->prev = P;
-				print_list(*list);
-				P = N->prev;
-				if (P)
-					N = P->next;
-			}
-			Element = Element->next;
-		}
-	}
+	/* while (h != NULL) */
+	/* { */
+	/* 	n++; */
+	/* 	h = h->next; */
+	/* } */
+	/* if (n >= 2) */
+	/* { */
+	/* 	Element = (*list)->next; */
+	/* 	while (Element) */
+	/* 	{ */
+	/* 		P = Element->prev; */
+	/* 		N = P->next; */
+	/* 		while (P && P->n > Element->n) */
+	/* 		{ */
+	/* 			Tmp = N->next; */
+	/* 			N->next = P; */
+	/* 			P->next = Tmp; */
+	/* 			Tmp = P->prev; */
+	/* 			P->prev = N; */
+	/* 			N->prev = Tmp; */
+	/* 			if (!N->prev) */
+	/* 				(*list) = N; */
+	/* 			else */
+	/* 				N->prev->next = N; */
+	/* 			if (P->next) */
+	/* 				P->next->prev = P; */
+	/* 			print_list(*list); */
+	/* 			P = N->prev; */
+	/* 			if (P) */
+	/* 				N = P->next; */
+	/* 		} */
+	/* 		Element = Element->next; */
+	/* 	} */
+	/* } */
 }
